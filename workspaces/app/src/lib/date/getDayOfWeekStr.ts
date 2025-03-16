168bfe858fa50508343acdb2cc7de479931ddca4
ciffelia <15273128+ciffelia@users.noreply.github.com>
2025-03-17 00:35:42 +0900

    remove moment-timezone

--- workspaces/app/src/lib/date/getDayOfWeekStr.ts
+++ workspaces/app/src/lib/date/getDayOfWeekStr.ts
@@ -1,12 +1,5 @@
-import type moment from 'moment-timezone';
-
 const days = ['sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday'] as const;
 
-export const getDayOfWeekStr = (date: moment.Moment) => {
-  const dayOfWeek = date.day();
-  const dayStr = days.at(dayOfWeek);
-  if (dayStr == null) {
-    throw new Error('dayOfWeek is invalid');
-  }
-  return dayStr;
+export const getDayOfWeekStr = () => {
+  return days[new Date().getDay()] as string;
 };
